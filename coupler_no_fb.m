b2f4f06e2f8c277985b2c9ff00d3b8b6afb3d662
Jipu Wang <jipuwang@umich.edu>
2017-06-04 15:25:58 -0400

    added mat parameter to include fission xs and thermal conductivity

--- coupler_no_fb.m
+++ coupler_no_fb.m
@@ -25,7 +25,10 @@ function [phi0_j,T_j]=coupler_no_fb(J,N,Tau,mat,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
     field1 = 'Sig_ss_j';  value1 = ones(J,1)*0.5;
     field2 = 'nuSig_f_j';  value2 = ones(J,1)*0.2;
     field3 = 'Sig_t_j';  value3 = ones(J,1);
-    mat = struct(field1,value1,field2,value2,field3,value3);
+    field4 = 'thermal_cond_k_j'; value4 = ones(J,1);
+    field5 = 'Sig_f_j'; value5 = ones(J,1)*0.1;
+    mat = struct(field1,value1,field2,value2,field3,value3,... 
+      field4,value4,field5,value5);
   end
   if ~exist('psi_b1_n','var')
     psi_b1_n=ones(N,1)*1.0; % the first/negative half is not useful; n=N/2+1:N % mu>0
@@ -58,6 +61,6 @@ function [phi0_j,T_j]=coupler_no_fb(J,N,Tau,mat,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
   pTriplePrime_j=1*0.1*phi0_j; % kappa=1, sigma_f=0.1;
 
 %% Call the heat conduction module to get the temperature
-  T_j=heat_cond_module(J,Tau,T_L,T_R,pTriplePrime_j,p_MMS_j);
+  T_j=heat_cond_module(J,Tau,mat,T_L,T_R,pTriplePrime_j,p_MMS_j);
 
 end
