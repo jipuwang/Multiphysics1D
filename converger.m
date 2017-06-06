2c1d38a9777037b73934433ebed3899a6d5a89ee
Jipu Wang <jipuwang@umich.edu>
2017-06-05 20:20:17 -0400

    changed the function call to conform to the change of manufacturer interface

--- converger.m
+++ converger.m
@@ -25,54 +25,36 @@ for iGrid=1:nGrids
   J=5*refinement^iGrid;
   gridMeshSize_n(iGrid)=Tau/J;
   
-    % Material
-    field1='Sig_t_j';          value1=ones(J,1);
-    field2='Sig_ss_j';         value2=ones(J,1)*0.5;
-    field3='Sig_gamma_j';      value3=ones(J,1)*0.4;
-    field4='Sig_f_j';          value4=ones(J,1)*0.1;
-    field5='nuSig_f_j';        value5=ones(J,1)*0.2;
-    field6='thermal_cond_k_j'; value6=ones(J,1);
-    field7='kappaSig_f_j';     value7=ones(J,1)*0.1; % kappa=1.0;
-    mat = struct(field1,value1,field2,value2,field3,value3,... 
-      field4,value4,field5,value5,field6,value6,field7,value7);
-    
-    hasFeedback=1;
-    manufacturer='sine_sine';
-    % no feedback
-    if hasFeedback
-      % call the manufacturer to get MMS problem and solution
-      switch(manufacturer)
-        case('const_quadratic')
-          [phi0_j_ana,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
-            T_j_ana,T_L,T_R,p_MMS_j]=...
-            manufacturer_const_quadratic_fb(J,N,Tau,mat);
-        case('sine_sine')
-          [phi0_j_ana,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
-            T_j_ana,T_L,T_R,p_MMS_j]=...
-            manufacturer_sine_sine_fb(J,N,Tau,mat);
-      end
+  % Material
+  field1='Sig_t_j';          value1=ones(J,1);
+  field2='Sig_ss_j';         value2=ones(J,1)*0.5;
+  field3='Sig_gamma_j';      value3=ones(J,1)*0.4;
+  field4='Sig_f_j';          value4=ones(J,1)*0.1;
+  field5='nuSig_f_j';        value5=ones(J,1)*0.2;
+  field6='thermal_cond_k_j'; value6=ones(J,1);
+  field7='kappaSig_f_j';     value7=ones(J,1)*0.1; % kappa=1.0;
+  mat = struct(field1,value1,field2,value2,field3,value3,... 
+    field4,value4,field5,value5,field6,value6,field7,value7);
 
+  fbType='linear'; % other options: 'noFeedback', 'squareRoot'
+  assumedSoln='sine_sine'; % other option: 'const_quadratic'
+
+  % call the manufacturer to get MMS problem and solution
+  [phi0_j_ana,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
+        T_j_ana,T_L,T_R,p_MMS_j]=...
+        manufacturer(J,N,Tau,mat,assumedSoln,fbType);
+
+  switch fbType
+    case {'linear','squareRoot'}
       % call the coupler to solve the above manufactured problem
       [phi0_j,T_j]=coupler_fb(J,N,Tau,mat,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
               T_L,T_R,p_MMS_j);
-    else % no feedback
-      % call the manufacturer to get MMS problem and solution
-      switch(manufacturer)
-        case('const_quadratic')
-          [phi0_j_ana,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
-            T_j_ana,T_L,T_R,p_MMS_j]=...
-            manufacturer_const_quadratic(J,N,Tau,mat);
-        case('sine_sine')
-          [phi0_j_ana,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
-            T_j_ana,T_L,T_R,p_MMS_j]=...
-            manufacturer_sine_sine(J,N,Tau,mat);
-      end
-
+    case 'noFeedback'
       % call the coupler to solve the above manufactured problem
       [phi0_j,T_j]=coupler_no_fb(J,N,Tau,mat,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
               T_L,T_R,p_MMS_j);
-    end
-        
+  end
+  
   % Calculate the error compared to manufactured solution
   error_phi0_n(iGrid)=norm(phi0_j-phi0_j_ana,2)/sqrt(J);
   error_T_n(iGrid)=norm(T_j-T_j_ana,2)/sqrt(J);
