4de383e2a713bc1f8ee92930114e71fa9ab2b0f1
Jipu Wang <jipuwang@umich.edu>
2017-06-07 22:02:29 -0400

    added a driver to run all cases

---
+++ driver.m
@@ -0,0 +1,20 @@
+% Driver
+clear all; close all;
+% Case configure options
+% fbType options: 'noFeedback','linear','squareRootPlus1'
+% assumedSoln options: 'const_quadratic','sine_sine','sqrtPlus1_quadratic'
+% mocSrc options: 'flat_source','linear_source'
+fbTypeS=['noFeedback','linear','squareRootPlus1'];
+mocSrcS=['flat_source','linear_source'];
+assumedSolnS=['const_quadratic','sine_sine','sqrtPlus1_quadratic'];
+
+for i_fbType=1:size(fbTypeS,2)
+  for j_mocSrc=1:size(mocSrcS,2)
+    for k_assumedSoln=1:size(assumedSolnS,2)
+      fbType=fbTypeS(i_fbType);
+      mocSrc=mocSrcS(j_mocSrc);
+      assumedSoln=assumedSolnS(k_assumedSoln);
+      converger;
+    end
+  end
+end
